e2c9485479a3204ac1eb0d655e39f48370a97416
Anthony Pena <anthony.pena@outlook.fr>
2020-03-12 08:03:10 +0100

    chore: init micronaut project

---
+++ src/main/java/ghost/butler/Application.java
@@ -0,0 +1,10 @@
+package ghost.butler;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
